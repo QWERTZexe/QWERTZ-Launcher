cd81603382f08344f1c08d2c1f6c6bd3caef60df
QWERTZexe <109920359+QWERTZexe@users.noreply.github.com>
2023-03-05 11:30:30 +0100

    Deleted useless windows files

--- windows/minecraft_launcher_lib/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import command, install, account, microsoft_account, utils, forge, fabric, runtime, exceptions, types, microsoft_types
-__all__ = ["command", "install", "account", "microsoft_account", "utils", "forge", "fabric", "runtime", "exceptions", "types", "microsoft_types"]
